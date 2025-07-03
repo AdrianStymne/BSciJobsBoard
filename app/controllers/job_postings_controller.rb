76a50137a5a7c5e297d5beb66e845534e1cdf928
AdrianStymne <163299691+AdrianStymne@users.noreply.github.com>
2025-07-03 16:05:22 +0100

    Filters added

--- app/controllers/job_postings_controller.rb
+++ app/controllers/job_postings_controller.rb
@@ -1,6 +1,19 @@
 class JobPostingsController < ApplicationController
   def index
     @job_postings = JobPosting.approved.published.recent
+
+    # Apply country filter
+    if params[:country].present?
+      @job_postings = @job_postings.where(country: params[:country])
+    end
+
+    # Apply right to work filter
+    if params[:requires_right_to_work].present?
+      @job_postings = @job_postings.where(requires_right_to_work: params[:requires_right_to_work] == "true")
+    end
+
+    # Get unique countries for filter dropdown (only from approved jobs)
+    @available_countries = JobPosting.approved.published.distinct.pluck(:country).compact.sort
   end
 
   def show
